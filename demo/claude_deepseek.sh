794494228ec4c4a889d7d25d9292a6bcad400396
Ming.Huang <means789@gmail.com>
2026-09-24 22:50:57 +0100

    start up

--- demo/claude_deepseek.sh
+++ demo/claude_deepseek.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-exec /mnt/e/code/_teamwork/_teamwork_system/.claude/skills/admin__util__team_startup/startup/claude_deepseek.sh "$@"
+/mnt/e/code/.code_setup/cmd_startup/startup_templates/sync_template.sh
+exec /mnt/e/code/.code_setup/cmd_startup/startup/claude_deepseek.sh "$@"
